8a7328e54d7002cdb71d83ead9ade2cb9409094a
Nitesh Seram <niteshseram@gmail.com>
2024-04-03 00:29:53 +0530

    Challenge 1

---
+++ app/components/button.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+
+type Props = Readonly<{
+  label: string;
+  variant: "primary" | "secondary";
+  className?: string;
+}>;
+
+export default function Button({ label, variant, className }: Props) {
+  return (
+    <button
+      className={clsx(
+        "text-base md:text-lg font-medium",
+        "rounded md:p-4 p-3 shadow",
+        "lg:w-44 md:w-52 w-full",
+        variant === "primary" && "bg-indigo-700 text-white",
+        className
+      )}
+    >
+      {label}
+    </button>
+  );
+}
