d17ffb121308a906421098ff91dc2503d6dc8e95
Nitesh Seram <niteshseram@gmail.com>
2024-04-05 23:29:16 +0530

    Add background color to stats card

--- components/stats-card.tsx
+++ components/stats-card.tsx
@@ -10,8 +10,8 @@ export default function StatsCard({ count, label }: Props) {
     <div
       className={clsx(
         "flex flex-col gap-4 items-center justify-center w-full text-center",
-        "border border-neutral-200 rounded-lg shadow",
-        "md:h-[140px] h-[132px]"
+        "border border-neutral-200 rounded-lg shadow bg-white",
+        "h-[132px] md:h-[140px]"
       )}
     >
       <h1 className="text-4xl md:text-5xl font-bold text-indigo-700">
